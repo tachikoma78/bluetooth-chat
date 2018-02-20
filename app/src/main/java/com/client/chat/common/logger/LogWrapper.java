0b0d17a1f746f92e47ae267c1c6103eb85b51aba
Pierre <pierrevanderpol@gmail.com>
2018-02-20 11:04:20 +0100

    log

--- app/src/main/java/com/client/chat/common/logger/LogWrapper.java
+++ app/src/main/java/com/client/chat/common/logger/LogWrapper.java
@@ -1,8 +1,57 @@
 package com.client.chat.common.logger;
 
+import android.util.Log;
+
 /**
- *
+ * wraps Android's native Log utility in the Logger interface.
  */
 
-public class LogWrapper {
+public class LogWrapper implements LogNode {
+    private LogNode mNext;
+
+    /**
+     * Returns the next LogNode in the linked list.
+     */
+    public LogNode getNext() {
+        return mNext;
+    }
+
+    /**
+     * Sets the LogNode data will be sent to..
+     */
+    public void setNext(LogNode node) {
+        mNext = node;
+    }
+
+
+    /**
+     * Prints data out to the console using Android's native log mechanism.
+     * @param priority Log level of the data being logged.  Verbose, Error, etc.
+     * @param tag Tag for for the log data.  Can be used to organize log statements.
+     * @param msg The actual message to be logged. The actual message to be logged.
+     * @param tr If an exception was thrown, this can be sent along for the logging facilities
+     *           to extract and print useful information.
+     */
+    @Override
+    public void println(int priority, String tag, String msg, Throwable tr) {
+        // Some logs method do not take msg parameter. Set to null.
+        String useMsg = msg;
+        if (useMsg == null) {
+            useMsg = "";
+        }
+
+        // If an exeption was provided, convert that exception to a usable string and attach
+        // it to the end of the msg method.
+        if (tr != null) {
+            msg += "\n" + Log.getStackTraceString(tr);
+        }
+
+        // This is functionally identical to Log.x(tag, useMsg);
+        Log.println(priority, tag, useMsg);
+
+        // If this isn't the last node in the chain, move things along.
+        if (mNext != null) {
+            mNext.println(priority, tag, msg, tr);
+        }
+    }
 }
